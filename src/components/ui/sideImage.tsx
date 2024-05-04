04541c9d9b92eaedb74d73df351cfd66d71f212e
DushDev <imthedush@gmail.com>
2024-05-04 14:08:40 +0300

    login page styles

---
+++ src/components/ui/sideImage.tsx
@@ -0,0 +1,16 @@
+export default function SideImage({ title }: { title: string }) {
+  return (
+    <div className="relative">
+      <img
+        src="/pawel-czerwinski-JEV7CrJTUNE-unsplash 1.png"
+        alt="Image"
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center">
+        <div className="font-bold transform -rotate-90 text-8xl text-white">
+          {title}
+        </div>
+      </div>
+    </div>
+  );
+}
